e3acd0ce9087e8e101cb6796441bc2710d181a3f
farzin89 <farzin_ahmadi90@yahoo.com>
2022-08-02 16:54:12 +0200

    Becoming one with the data and visualising a text dataset

--- NLP Fundamentals in TensorFLow/NLP Fundamental in TensorFlow.py
+++ NLP Fundamentals in TensorFLow/NLP Fundamental in TensorFlow.py
@@ -36,3 +36,45 @@ see the original source here: https://www.kaggle.com/competitions/nlp-getting-st
 # Unzip data
 
 unzip_data("nlp_getting_started.zip")
+
+"""## Visualizing a text dataset 
+
+To visualize our text samples, we first have to read them in, one way to do so would be to use Python : https://realpython.com/read-write-files-python/
+
+But I prefer to get visual straight away. 
+So another way to do this is to use pandas.. 
+"""
+
+import pandas as pd
+
+train_df = pd.read_csv("train.csv")
+test_df = pd.read_csv("test.csv")
+train_df.head()
+
+train_df["text"][1]
+
+# shuffle training dataframe
+
+train_df_shuffled = train_df.sample(frac=1,random_state=42)
+train_df_shuffled.head()
+
+# What does the test dataframe look like?
+
+test_df.head()
+
+# How many examples of each class?
+train_df.target.value_counts()
+
+# How many total samples?
+
+len(train_df),len(test_df)
+
+# Let's visualize some random training examples
+
+import random
+random_index = random.randint(0,len(train_df)-5) # create random indexs not higher than the total number of samples
+for row in train_df_shuffled[["text","target"]][random_index:random_index+ 5 ].itertuples():
+  _,text,target = row
+  print(f"Target: {target}","(real disaster)" if target > 0 else "(not real disaster)")
+  print(f"Text:\n{text}\n")
+  print("---\n")
